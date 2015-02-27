6ad6c50edb4b47068fdbfb7f0c25496622d1510f
SmexyShimala <jshimala@saintjoe.edu>
2015-02-27 13:04:24 -0600

    Update AttrDriver.java

--- AttrDriver.java
+++ AttrDriver.java
@@ -1,1 +1,10 @@
-
+class ColorAttr extends Attr {
+  private SreenColor myColor;
+  
+  public ColorAttr(String name, Object value) {
+    super(name, value);
+    decodeColor();
+  }
+  
+  public ColorAttr(String name)
+}
